27538becfe199ec388c412d7dc7a845e9c3a27d3
MiguelCastilloSanchez <A18003592@alumnos.uady.mx>
2024-07-23 22:03:58 -0600

    initial code for  user authentication service

---
+++ backend/auth-service/src/main/java/com/example/auth_service/service/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.example.auth_service.service;
+
+import com.example.auth_service.model.User;
+import com.example.auth_service.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities("USER")
+                .build();
+    }
+}
