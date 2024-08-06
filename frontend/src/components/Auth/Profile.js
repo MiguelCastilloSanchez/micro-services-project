9829417e1bf7aacf91a176489f03b8c8edacb4e4
MiguelCastilloSanchez <A18003592@alumnos.uady.mx>
2024-08-06 10:54:50 -0600

    First steps to pfp

--- frontend/src/components/Auth/Profile.js
+++ frontend/src/components/Auth/Profile.js
@@ -1,13 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import UpdatePassword from './UpdatePassword';
 import { logout, deleteUser } from '../../api/auth';
 import { useNavigate } from 'react-router-dom';
-import { Button, Typography, Grid } from '@mui/material';
+import { Button, Typography, Grid, Avatar, StyledBadge } from '@mui/material';
+import { jwtDecode } from 'jwt-decode';
 
 const Profile = () => {
   const [token, setToken] = useState(localStorage.getItem('token'));
   const [error, setError] = useState('');
+  const [username, setUsername] = useState('');
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      const username = decodedToken.sub;
+      setUsername(username);
+    }
+  }, []);
+
+  const getInitial = (name) => {
+    if (name) {
+      return name.charAt(0).toUpperCase();
+    }
+    return '';
+  };
 
   const handleLogout = async () => {
     try {
@@ -46,10 +63,11 @@ const Profile = () => {
           alignItems: 'center',
           p: 2,
           m: 0,
-          gap: 4,
+          gap: 3,
         }}>
         <>
-          <Typography variant="h4" sx={{ flexGrow: 1, textAlign: 'center', fontWeight: 'bold' }}>My Profile</Typography>
+          <Avatar sx={{ width: 100, height: 100 }}>{getInitial(username)}</Avatar>
+          <Typography variant="h5" sx={{ flexGrow: 1, textAlign: 'center', fontWeight: 'bold' }}>{username}</Typography>
           <UpdatePassword token={token} onLogout={handleLogoutOnPasswordChange} />
           <Button onClick={handleLogout} variant="contained" color="primary" sx={{ backgroundColor: 'white', width: '180px', color: 'black' }}>
             Logout
